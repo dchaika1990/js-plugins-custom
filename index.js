e8cb03d835963d835c0ff889c7aaead8c14eb7f4
dchaika <dchaika1990@gmail.com>
2020-10-05 10:04:07 +0300

    show fruits and add model 'Show price'

--- index.js
+++ index.js
@@ -1,3 +1,41 @@
+const products = document.querySelector('.products')
+
+fruits.forEach( fruit => {
+    products.insertAdjacentElement('beforeend', card(fruit))
+} )
+
+/*
+*   1. Динамически вывести карточки товаров
+*   2. Показать цену в модалке (и это должна быть одна модалка)
+ */
+
+const singleProduct = [].slice.call(document.querySelectorAll('[data-card]'))
+singleProduct.forEach(product => {
+    const productTitle = product.querySelector('.card-title').textContent
+    const productPrice = product.getAttribute('data-price')
+    const handler = function(){
+        const priceModal = $.modal({
+            title: productTitle,
+            content: `
+                <p>Price: ${productPrice}</p>
+            `,
+            closable: true,
+            footerButtons: [
+                {
+                    text: 'Close', type: 'danger', handler() {
+                        priceModal.close()
+                        priceModal.destroy()
+                    }
+                }
+            ]
+        })
+        priceModal.open()
+    }
+    product.querySelector('.btn-primary').addEventListener('click', handler)
+})
+
+
+// Modal
 const modal = $.modal({
     title: 'New title',
     content: `
